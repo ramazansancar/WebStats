0d35b7a95afcd1f06367a4a8c132a165581856ca
Berk A <ege.berk1982@hotmail.com>
2016-09-07 18:16:28 +0300

    Upload :)

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php require_once 'settings.php'; ?>
+<!DOCTYPE html>
+<html lang="tr">
+	<head>
+		<meta charset="UTF-8">
+		<title><?php echo $set["site_name"]; ?></title>
+		<link rel='stylesheet' href='css/bootstrap.css'> 
+	</head>
+	<body>
+		<div class="navbar navbar-default navbar-fixed-top">
+	  <div class="container">
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+
+	      </button> <a class="navbar-brand" href="#"><?php echo $set["site_name"]; ?></a>
+
+	    </div>
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <!-- [Sağ Menü] -->
+	      <ul class="nav navbar-nav">
+	        <li class="active"><a href="index.php">Home</a></li>
+	      </ul>
+	      <!-- [Sol Menü] -->
+	  	<script type="text/javascript">
+			document.write(unescape('%3C%75%6C%20%63%6C%61%73%73%3D%22%6E%61%76%20%6E%61%76%62%61%72%2D%6E%61%76%20%6E%61%76%62%61%72%2D%72%69%67%68%74%22%3E%0A%20%20%20%20%20%20%20%20%3C%6C%69%3E%3C%61%20%68%72%65%66%3D%22%68%74%74%70%3A%2F%2F%62%65%72%6B%2E%70%77%22%3E%40%42%65%72%6B%50%57%3C%2F%61%3E%3C%2F%6C%69%3E%0A%20%20%20%20%20%20%3C%2F%75%6C%3E'));
+		</script>
+	    </div>
+	  </div>
+	</div>
+
+
+	<div class="container" style="margin-top: 80px">
+	  <div class="panel panel-primary">
+	    <div class="panel-heading">
+	      <h3 class="panel-title">Top <?php echo $set["table_limit"]; ?></h3>
+	    </div>
+	    <table class="table table-striped table-hover ">
+	      <thead>
+	        <tr>
+	          <th>#</th>
+	          <th>Oyuncu</th>
+	          <th>Öldürme</th>
+	          <?php echo ($set["mob_kill"] == "1") ? "<th>Mob Öldürme</th>" : null ; ?>
+	          <th>Ölüm</th>
+	        </tr>
+	      </thead>
+	      <tbody>
+			<?php 
+
+				$query = $db->query("SELECT * FROM player_stats ORDER BY kills DESC LIMIT 0,".$set["table_limit"], PDO::FETCH_ASSOC);
+				if ( $query->rowCount() ){
+			     	foreach( $query as $row ){
+						$head = ($set["head"] == "1") ? '<img src="https://minotar.net/avatar/'.$row["playername"].'/17" class="img-circle">' : null ;
+						echo "<tr>";
+						echo "<td>".$row["id"]."</td>";
+						echo "<td>".$head." ".$row["playername"]."</td>";
+						echo "<td>".$row["kills"]."</td>";
+						echo ($set["mob_kill"] == "1") ? "<td>".$row["mobkills"]."</td>" : null;
+						echo "<td>".$row["deaths"]."</td>";
+						echo "</tr>";
+			     	}
+				}
+
+			?>
+	      </tbody>
+	    </table>
+	  </div>
+	</div>
+
+	<script src="js/jquery-2.2.4.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+	</body>
+</html>
---
+++ settings.php
@@ -0,0 +1,33 @@
+<?php
+
+$set = array(
+	'site_name' => 'PWCraft WebStats',
+	'mob_kill' => '1' /* 1 = Mob kill [ON] || 0 = Mob kill [OFF] */,
+	'table_limit' => '10' /* Table line limit */,
+	'head' => '1', /* 1 = Player Head [ON] || 0 = Player Head [OFF] */
+	'mysql' => array(
+		'host' => 'localhost',
+		'username' => 'root',
+		'password' => 'root',
+		'database' => 'stt',
+	)
+);
+
+#######################
+## Database Settings ##
+#######################
+
+$host = $set["mysql"]["host"];
+$username = $set["mysql"]["username"];
+$password = $set["mysql"]["password"];
+$database = $set["mysql"]["database"];
+
+try {
+     $db = new PDO("mysql:host=$host;dbname=$database", "$username", "$password");
+     $db->query("SET CHARACTER SET uf8");
+} catch ( PDOException $e ){
+     print $e->getMessage();
+     die();
+}
+
+?>
