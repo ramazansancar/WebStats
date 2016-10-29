08683f56a1a63dc70f360707646ca856298f6d5f
Berk A <ege.berk1982@hotmail.com>
2016-10-29 10:44:45 +0300

    CHARACTER SET (Edit)

--- settings.php
+++ settings.php
@@ -27,7 +27,7 @@ $database = $set["mysql"]["database"];
 
 try {
      $db = new PDO("mysql:host=$host;dbname=$database", "$username", "$password");
-     $db->query("SET CHARACTER SET uf8");
+     $db->query("SET CHARACTER SET utf8");
 } catch ( PDOException $e ){
      print $e->getMessage();
      die();
